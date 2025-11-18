79c5a85d81cefc6476b14b04381afe6b2af523a7
kamal singh <kamalsinghbs347@gmail.com>
2025-11-18 22:32:36 +0530

    Added new Feature in sum

---
+++ avg.java
@@ -0,0 +1,9 @@
+
+class avg {
+
+    public static void main(String[] asf) {
+        int a = 10, b = 20, c = 30;
+        int avg = (a + b + c) / 3;
+        System.out.print("Average: " + avg);
+    }
+}
