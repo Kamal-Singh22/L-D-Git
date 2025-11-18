748f573bab504a9368143acf309b738c858ea325
kamal singh <kamalsinghbs347@gmail.com>
2025-11-18 22:07:20 +0530

    Update the file

--- helllo.java
+++ helllo.java
@@ -3,5 +3,9 @@ class helllo {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int a = 10;
+        int b = 15;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
     }
 }
